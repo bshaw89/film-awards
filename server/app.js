54e5ded8b2cecf8d5446d2225d5e0ab5f886c379
Benjamin Shaw <shaw.j.benjamin@gmail.com>
2020-09-07 20:21:27 -0400

    Resolve api issue

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..','build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '..' ,'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..' ,'build', 'index.js'));
 });
 
 const server = http.createServer(app);
